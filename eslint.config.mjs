02f19fca3177d97c8722a26761a4929c8b3f4132
이영우 <zestlee1106@gmail.com>
2024-07-29 22:02:21 +0900

    lint 설정 추가

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      // 'prettier/prettier': 'error',
+      'react/prop-types': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+      'react/function-component-definition': [
+        2,
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-console': 'warn',
+      'global-require': 'off',
+      'react/react-in-jsx-scope': 'off'
+    },
+  },
+]
