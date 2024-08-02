82a2cbd0a09d8f145d0090eb364f443cdba9029e
이영우 <zestlee1106@gmail.com>
2024-08-02 22:51:11 +0900

    update: svgr 설정 추가
    - 빌드 에러나서 devDependencies 에서 dependencies 로 옮김
    - next config 에 불필요한 소스 삭제
    - svgr.d.ts 추가함

---
+++ types/svgr.d.ts
@@ -0,0 +1,12 @@
+import { FC, SVGProps, SVGSVGElement } from 'react'
+
+declare module '*.svg' {
+  import { FC, SVGProps } from 'react'
+  const content: FC<SVGProps<SVGElement>>
+  export default content
+}
+
+declare module '*.svg?url' {
+  const content: any
+  export default content
+}
