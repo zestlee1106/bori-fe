5fba3504ad7af15fdc2a543da5091d4b742ce838
이영우 <zestlee1106@gmail.com>
2024-08-05 22:16:28 +0900

    add: dotenv 추가 및 환경 변수 적용되게끔 next config 및 빌드 스크립트 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+/* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
+
+import fs from 'fs'
+import path from 'path'
+import dotenv from 'dotenv'
+
+const envFile = process.env.BUILD_ENV ? `.env.${process.env.BUILD_ENV}` : '.env'
+const envPath = path.resolve(process.cwd(), envFile)
+
+if (fs.existsSync(envPath)) {
+  dotenv.config({ path: envPath })
+}
+
 const nextConfig = {
   webpack(config) {
     config.module.rules.push({
@@ -27,6 +40,9 @@ const nextConfig = {
         hostname: 'images.unsplash.com',
       },
     ],
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.API_URL,
   },
 }
 
