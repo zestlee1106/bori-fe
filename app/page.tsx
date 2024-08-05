ff17b3a5b0087f1e9bf6b48ece06539bfd72d5ea
이영우 <zestlee1106@gmail.com>
2024-08-05 21:49:33 +0900

    update: home page 에서 Banner 컴포넌트를 사용하도록 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import RightArrow from '@/public/icon/right-arrow.svg'
 import Image from 'next/image'
+import Banner from './components/basic/Banner'
 
 // TODO: 응답 데이터로부터 가져오도록 수정
 const startColor = '#eec695'
@@ -9,31 +10,7 @@ const imageLink = 'https://bori-fe.vercel.app/icon/mock/house.png'
 const Home = () => {
   return (
     <div>
-      {/* 상단 영역 */}
-      <div className="pt-[8px]">
-        <div
-          style={{
-            background: `linear-gradient(-59deg, ${startColor}, ${endColor})`,
-          }}
-          className="rounded-[20px] relative drop-shadow-lg"
-        >
-          <div className="w-[342px] h-[168px] pt-[38px] pl-[27px] pr-[56px]">
-            <div className="font-extrabold text-[22px] tracking-[-0.55px] leading-[30px]">
-              우리 동네 보청기 판매점 정보를
-              <br />다 모았어요
-            </div>
-            <div className="flex items-center pt-[8px] text-GRAY tracking-[-0.55px] leading-[30px] text-[14px] font-semibold">
-              집 근처 보청기 판매점 찾는 방법
-              <div className="pl-[6.75px]">
-                <RightArrow width="4.91" height="8.49" fill="#767676" />
-              </div>
-            </div>
-            <div className="overflow-hidden absolute bottom-[21px] right-[28px] pl-[6px]">
-              <Image src={imageLink} alt="banner" width={99} height={68} objectFit="cover" />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Banner />
 
       {/* 중간 텍스트 영역 */}
       <div className="h-[53px] mt-[25px] flex items-center">
