cede31a519b0d5bea883873e63249d336ab4a755
zestlee1106 <zestlee1106@gmail.com>
2024-08-05 23:14:36 +0900

    테스트 수정

--- app/api/v1/banners/route.ts
+++ app/api/v1/banners/route.ts
@@ -2,7 +2,7 @@ import { banners } from '@/public/constants/mock'
 
 export async function GET() {
   console.log(
-    '%c 🤩🤩🤩 영우의 로그 API 요청: >>>',
+    '%c 🤩🤩🤩 영우의 로그 API 요청: ',
     'font-size: x-large; color: #bada55;',
     '',
     process.env.API_URL || process.env.NEXT_PUBLIC_API_URL,
