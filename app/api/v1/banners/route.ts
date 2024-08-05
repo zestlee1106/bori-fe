38fe488d03da29efb77114f14f14aa7ac7a41ea9
이영우 <zestlee1106@gmail.com>
2024-08-05 22:50:27 +0900

    test: 로그 추가

--- app/api/v1/banners/route.ts
+++ app/api/v1/banners/route.ts
@@ -1,6 +1,11 @@
 import { banners } from '@/public/constants/mock'
 
 export async function GET() {
-  console.log('%c 🤩🤩🤩 영우의 로그 API 요청: ', 'font-size: x-large; color: #bada55;', '')
+  console.log(
+    '%c 🤩🤩🤩 영우의 로그 API 요청: ',
+    'font-size: x-large; color: #bada55;',
+    '',
+    process.env.NEXT_PUBLIC_API_URL,
+  )
   return Response.json({ banners: banners })
 }
