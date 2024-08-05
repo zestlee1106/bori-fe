f98b49f56cf7fba1b32654b2f400a3d648595467
이영우 <zestlee1106@gmail.com>
2024-08-05 23:12:21 +0900

    로그 수정

--- app/api/v1/banners/route.ts
+++ app/api/v1/banners/route.ts
@@ -2,7 +2,7 @@ import { banners } from '@/public/constants/mock'
 
 export async function GET() {
   console.log(
-    '%c 🤩🤩🤩 영우의 로그 API 요청: ',
+    '%c 🤩🤩🤩 영우의 로그 API 요청: >>>',
     'font-size: x-large; color: #bada55;',
     '',
     process.env.API_URL || process.env.NEXT_PUBLIC_API_URL,
