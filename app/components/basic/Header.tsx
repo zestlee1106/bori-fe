d01363aa0aa4779052f8341edcc01dc08a532b99
zestlee1106 <zestlee1106@gmail.com>
2024-08-12 22:49:25 +0900

    update: 판매점 목록 페이지 헤더를 추가하면서 헤더의 설정을 수정함
    - 헤더의 오른쪽 영역에 헤더 아이템에 대한 엘리먼트, 클릭 핸들러를 추가할 수 있도록 함
    - 뒤로가기 이벤트는 그대로 넣되, 오른쪽 영역 클릭 이벤트는 따로 처리할 수가 없을 것 같아서 헤더 컴포넌트 내에서 처리하도록 일단은 해놓음

--- app/components/basic/Header.tsx
+++ app/components/basic/Header.tsx
@@ -7,13 +7,19 @@ import Image from 'next/image'
 import Logo from '@/public/icon/logo.png'
 import HomeIcon from '@/public/icon/home.svg'
 import LeftArrowIcon from '@/public/icon/left-arrow.svg'
+import { useRouter } from 'next/navigation'
 
-interface HeaderState {
+interface HeaderItem {
+  element: React.JSX.Element
+  onClick?: 'navigateHome' | (() => void)
+}
+
+interface HeaderConfig {
   backgroundColor: 'transparent' | 'white' | 'black'
   position: 'fixed' | 'absolute'
   left: 'logo' | 'back'
   center: string | React.JSX.Element | null
-  right: 'hamburger' | React.JSX.Element | null
+  right: HeaderItem | null
 }
 
 const ShopTitle = () => {
@@ -24,7 +30,7 @@ const ShopTitle = () => {
   )
 }
 
-const HEADER_CONFIG: Record<string, HeaderState> = {
+const HEADER_CONFIG: Record<string, HeaderConfig> = {
   '/': {
     backgroundColor: 'transparent',
     position: 'absolute',
@@ -38,17 +44,46 @@ const HEADER_CONFIG: Record<string, HeaderState> = {
     position: 'fixed',
     left: 'back',
     center: <ShopTitle />,
-    right: <HomeIcon />,
+    right: {
+      element: <HomeIcon />,
+      onClick: 'navigateHome',
+    },
   },
 }
 
 const Header = () => {
   const pathname = usePathname()
+  const router = useRouter()
 
   const { left, center, right, backgroundColor, position } = HEADER_CONFIG[pathname] || {
-    showLogo: true,
+    left: 'logo',
+    center: null,
+    right: null,
     backgroundColor: 'transparent',
-    showBackButton: false,
+    position: 'absolute',
+  }
+
+  /**
+   * 뒤로가기 버튼 이벤트 핸들러
+   */
+  const handleBackClick = () => {
+    router.back()
+  }
+
+  /**
+   * 오른쪽 버튼 이벤트 핸들러
+   * @description onClick에 설정된 동작을 실행함
+   */
+  const handleRightClick = () => {
+    if (!right) {
+      return false
+    }
+
+    if (right.onClick === 'navigateHome') {
+      router.push('/')
+    } else {
+      right.onClick?.()
+    }
   }
 
   return (
@@ -64,12 +99,16 @@ const Header = () => {
       <div className="flex justify-between items-center w-full">
         {left === 'logo' && <Image className="w-[50px] h-[50px]" src={Logo} alt="logo" />}
         {left === 'back' && (
-          <div className="justify-start">
+          <div className="justify-start" onClick={handleBackClick}>
             <LeftArrowIcon />
           </div>
         )}
         {center && <div>{center}</div>}
-        {right && <div className="justify-end">{right}</div>}
+        {right && (
+          <div className="justify-end" onClick={handleRightClick}>
+            {right.element}
+          </div>
+        )}
       </div>
     </header>
   )
