79a91bb8804e34a60f80aadc562a59b079b738e0
zestlee1106 <zestlee1106@gmail.com>
2024-08-14 22:37:59 +0900

    temp: 목록 페이지에서 임시로 토스트를 보여주게끔 해놓음

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect } from 'react'
+import useToast from '@/app/hooks/toast'
+
+const ToastMessage = () => {
+  return (
+    <>
+      <span className="font-bold">전화번호</span> 가 복사되었습니다.
+    </>
+  )
+}
 
 const page = () => {
+  const { toast } = useToast()
+
+  useEffect(() => {
+    toast(<ToastMessage />)
+  }, [])
+
   return <div>page</div>
 }
 
