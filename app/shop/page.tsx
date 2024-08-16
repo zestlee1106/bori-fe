cd8111bb3decf887730e5c720dcfbeed50f8a570
zestlee1106 <zestlee1106@gmail.com>
2024-08-16 23:13:07 +0900

    update: page 단에서는 use client 안 쓰도록 수정

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,15 +1,11 @@
-'use client'
-
 import React from 'react'
 import Shop from '@/app/components/container/Shop'
+import ShopHeader from '@/app/components/basic/ShopHeader'
 
 const page = () => {
   return (
     <div>
-      <div className="flex fixed top-header mt-[10px] h-[33px z-[8000]">
-        <div className="w-[85px] h-[23px] py-[5px] bg-GREEN_800 items-center flex">지역선택</div>
-        <div>지역</div>
-      </div>
+      <ShopHeader />
       <div className="pt-[57px] flex flex-col gap-[12px]">
         <Shop />
         <Shop />
