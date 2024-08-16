71288870b1c508746a2618037360cf6d9908acef
zestlee1106 <zestlee1106@gmail.com>
2024-08-16 17:09:14 +0900

    add: 토스트 메신지 트랜지션 추가

--- app/components/basic/Toast.tsx
+++ app/components/basic/Toast.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useToastStore from '@/app/stores/toast'
 import { Toast as ToastStore } from '@/app/stores/toast'
-
-const DEFAULT_DURATION = 100000
+import { DEFAULT_DURATION } from '@/app/components/container/ToastList'
 
 interface IToastProps {
   toast: ToastStore
@@ -15,24 +14,31 @@ const Toast = ({ toast }: IToastProps) => {
 
   const { removeToast } = useToastStore()
 
-  const index = Number(id.replace('toast-', ''))
-  const zIndex = TOAST_DEFAULT_Z_INDEX + index
+  const toastIndex = Number(id.replace('toast-', ''))
+  const calculatedZIndex = TOAST_DEFAULT_Z_INDEX + toastIndex
 
-  setTimeout(() => {
-    removeToast(id)
-  }, duration || DEFAULT_DURATION)
+  // 토스트메시지가 나오고 바로 삭제되면 트랜지션이 보이지 않아서 0.5 초 추가
+  const toastDuration = (duration || DEFAULT_DURATION) + 500
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(id)
+    }, toastDuration)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
 
   return (
-    <>
-      <div
-        className="bg-BLACK_900 rounded-[10px] h-[42px] text-WHITE_1000 py-[11px] pl-[22px] text-[14px] leading-[20px] tracking-[-0.025em] absolute left-0 right-0 mx-auto w-[calc(100%-48px)] bottom-0"
-        style={{
-          zIndex,
-        }}
-      >
-        {text}
-      </div>
-    </>
+    <div
+      className="bg-BLACK_900 rounded-[10px] h-[42px] text-WHITE_1000 py-[11px] pl-[22px] text-[14px] leading-[20px] tracking-[-0.025em] absolute left-0 right-0 mx-auto w-[calc(100%-48px)] bottom-0"
+      style={{
+        zIndex: calculatedZIndex,
+      }}
+    >
+      {text}
+    </div>
   )
 }
 
--- app/components/container/ToastList.tsx
+++ app/components/container/ToastList.tsx
@@ -1,11 +1,34 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React, { CSSProperties, useEffect, useState } from 'react'
 import useToastStore from '@/app/stores/toast'
 import ReactDOM from 'react-dom'
 import Toast from '@/app/components/basic/Toast'
+import { Transition, TransitionGroup } from 'react-transition-group'
 
-const ToastList = () => {
+export const DEFAULT_DURATION = 1000
+
+const getTransitionStyles = (duration: number = DEFAULT_DURATION): Record<string, CSSProperties> => {
+  return {
+    entering: {
+      position: 'absolute',
+      opacity: 0,
+      transform: 'translateY(100px)',
+    },
+    entered: {
+      opacity: 1,
+      transition: `opacity ${duration / 2}ms ease-in-out, transform ${duration}ms ease-in-out`,
+      transform: 'translateY(0)',
+    },
+    exiting: {
+      opacity: 0,
+      transition: `opacity ${duration}ms ease-in-out, transform ${duration}ms ease-in-out`,
+      transform: 'translateY(100px)',
+    },
+  }
+}
+
+const ToastList: React.FC = () => {
   const { toastList } = useToastStore()
   const [mounted, setMounted] = useState(false)
 
@@ -24,11 +47,23 @@ const ToastList = () => {
   }
 
   return ReactDOM.createPortal(
-    <div>
+    <TransitionGroup>
       {toastList.map((toast) => (
-        <Toast key={toast.id} toast={toast} />
+        <Transition
+          key={toast.id}
+          timeout={{
+            // 토스트메시지가 사라지기까지 시간 (현재는 보이고서 1초 대기)
+            exit: 1000,
+          }}
+        >
+          {(status) => (
+            <div style={getTransitionStyles(toast.duration)[status]}>
+              <Toast toast={toast} />
+            </div>
+          )}
+        </Transition>
       ))}
-    </div>,
+    </TransitionGroup>,
     toastRoot,
   )
 }
