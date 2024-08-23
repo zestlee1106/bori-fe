dee0d31432cfb932e35f5ebfe93dc3961846c7d5
zestlee1106 <zestlee1106@gmail.com>
2024-08-23 22:17:52 +0900

    refactor: chip 컴포넌트로 빼냄

--- app/components/basic/ShopHeader.tsx
+++ app/components/basic/ShopHeader.tsx
@@ -5,32 +5,32 @@ import React, { useEffect } from 'react'
 import SettingIcon from '@/public/icon/setting.svg'
 import DownArrowIcon from '@/public/icon/down-arrow.svg'
 
+const Chip = ({ children }: { children: React.ReactNode }) => (
+  <div className="bg-GREY_200 h-[2rem] text-[1rem] font-bold text-GREY_700 py-[0.4rem] px-[1rem] leading-[1.193rem] tracking-[-0.025em] rounded-[5rem] shadow-CARD_SHADOW">
+    {children}
+  </div>
+)
+
+const HeaderContent = () => (
+  <div className="flex mt-[1rem] z-[8000] px-[0.9rem] items-end">
+    <div className="h-[2.3rem] px-[1.2rem] py-[0.5rem] bg-GREEN_800 items-center flex leading-[1.313rem] tracking-[-0.025em] font-bold text-WHITE_1000 text-[1.1rem] rounded-[5rem] gap-[0.55rem] mr-[1.6rem] shadow-CARD_SHADOW">
+      <SettingIcon />
+      지역선택
+      <DownArrowIcon />
+    </div>
+    <div className="flex gap-[1.1rem]">
+      <Chip>전국</Chip>
+      <Chip>강남구</Chip>
+      <Chip>송파구</Chip>
+    </div>
+  </div>
+)
+
 const ShopHeader = () => {
   const { setAdditionalHeaderContent } = useHeaderStore()
 
-  const Header = () => (
-    <div className="flex mt-[1rem] z-[8000] px-[0.9rem] items-end">
-      <div className="h-[2.3rem] px-[1.2rem] py-[0.5rem] bg-GREEN_800 items-center flex leading-[1.313rem] tracking-[-0.025em] font-bold text-WHITE_1000 text-[1.1rem] rounded-[5rem] gap-[0.55rem] mr-[1.6rem] shadow-CARD_SHADOW">
-        <SettingIcon />
-        지역선택
-        <DownArrowIcon />
-      </div>
-      <div className="flex gap-[1.1rem]">
-        <div className="bg-GREY_200 h-[2rem] text-[1rem] font-bold text-GREY_700 py-[0.4rem] px-[1rem] leading-[1.193rem] tracking-[-0.025em] rounded-[5rem] shadow-CARD_SHADOW">
-          전국
-        </div>
-        <div className="bg-GREY_200 h-[2rem] text-[1rem] font-bold text-GREY_700 py-[0.4rem] px-[1rem] leading-[1.193rem] tracking-[-0.025em] rounded-[5rem] shadow-CARD_SHADOW">
-          강남구
-        </div>
-        <div className="bg-GREY_200 h-[2rem] text-[1rem] font-bold text-GREY_700 py-[0.4rem] px-[1rem] leading-[1.193rem] tracking-[-0.025em] rounded-[5rem] shadow-CARD_SHADOW">
-          송파구
-        </div>
-      </div>
-    </div>
-  )
-
   useEffect(() => {
-    setAdditionalHeaderContent(<Header />)
+    setAdditionalHeaderContent(<HeaderContent />)
     const originOverflow = document.body.style.overflow
     document.body.style.overflow = 'hidden'
 
@@ -38,7 +38,7 @@ const ShopHeader = () => {
       setAdditionalHeaderContent(null)
       document.body.style.overflow = originOverflow
     }
-  }, [])
+  }, [setAdditionalHeaderContent])
 
   return null
 }
